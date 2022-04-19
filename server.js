b2f1cf545e1508c21e475f115db8d37628877b98
{Daiki <{azano0417@yahoo.co.jp}>
2022-04-20 03:48:09 +0900

    test

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const socketIO = require('socket.io');
+const app = express();
+var http = require('http').Server(app);
+const socketIO = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 var test = 0;
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-  const io = socketIO(server);
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+app.listen(app.get('port'), function() {
+  console.log('listening on *:' + app.get('port'));
+});
+  const io = socketIO(http);
 
   io.on("connection", (socket)=>{
     console.log("ユーザーが接続しました");
