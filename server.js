68b2d21a7ee42ab7f6b83cd9c46f950561540f8f
{Daiki <{azano0417@yahoo.co.jp}>
2022-04-20 03:32:31 +0900

    test

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 var test = 0;
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-  server.use(express.static(__dirname + '/public'));
+const server = express();
+  server.use((req, res) => res.sendFile(INDEX, { root: __dirname }));
+  server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
   const io = socketIO(server);
 
