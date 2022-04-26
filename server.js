ae63e0bf6e8cf3c59e2e609d3ff6a795d5199443
{Daiki <{azano0417@yahoo.co.jp}>
2022-04-27 01:14:45 +0900

    id_test

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const express = require('express');
 const socketIO = require('socket.io');
 
@@ -6,15 +7,28 @@ const INDEX = '/index.html';
 var test = 0;
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-  const io = socketIO(server);
+const io = socketIO(server);
 
-  io.on("connection", (socket)=>{
-    console.log("ユーザーが接続しました");
+io.on("connection", (socket)=>{
+  console.log("ユーザーが接続しました");
 
-    socket.on("shake", (nm)=>{
-      io.emit("member-shake", nm);
-    });
+  (()=>{
+    // トークンを作成
+    const token = makeToken(socket.id);
+
+    // 本人にトークンを送付
+    io.to(socket.id).emit("token", {token:token});
+  })();
+  socket.on("shake", (nm,ttoken)=>{
+    io.emit("member-shake", nm,ttoken);
   });
+});
+
+
+function makeToken(id){
+  const str = "qawsedrftgyhujiko" + id;
+  return( crypto.createHash("sha1").update(str).digest('hex') );
+}
