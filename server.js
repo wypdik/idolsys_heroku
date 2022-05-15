762672c2d24b4a929102ce14ec00b8ff0cb2f531
{Daiki <{azano0417@yahoo.co.jp}>
2022-05-16 00:08:40 +0900

    glowstick

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
-var test = 0;
+var member_count_server = 0;
 
 const server = express()
 .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
@@ -13,6 +13,7 @@ const server = express()
 const io = socketIO(server);
 
 io.on("connection", (socket)=>{
+  member_count_server ++;
   console.log("ユーザーが接続しました");
 
   (()=>{
